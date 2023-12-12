c734319d180b2b85ca118421c69d92882e36f12f
JJAHMEDAMER <jjahmedamer@gmail.com>
2023-12-12 15:30:20 +0200

    refactor file to module

--- main.py
+++ main.py
@@ -3,55 +3,68 @@ from openpyxl import Workbook
 import re
 import os
 
-OUTPUT_DIR = "./output"
 
-reader = PdfReader("Egypt foods - TQM Certificates.pdf")
-wb = Workbook()
-ws = wb.active
-
-issue_date = "9/7/2023"
-company_name = "Egypt Foods Group"
-certificate_field = "Total Quality Management Training (TQM)"
-
-titles = [
-    "certificate_holder",
-    "certificate_code",
-    "certificate_field",
-    "issue_date",
-    "company_name",
-]
-
-ws.append(titles)
-
-for page in reader.pages:
-    text = page.extract_text()
-
-    # Cert Holder
-    exp = re.compile(r"It is hereby certified that\s*(.*?)\s*Attended the training on")
-    certificate_holder = exp.findall(text)
-    print(certificate_holder)
-
-    # Cert Code
-    exp = re.compile(r"\b[A-Z]{2}-[A-Z]{3}-\d{3}-\d{4}-\d{2}\b")
-    certificate_code = exp.findall(text)
-    ws.append(
-        [
-            certificate_holder[0],
-            certificate_code[0],
-            certificate_field,
-            issue_date,
-            company_name,
-        ]
-    )
-
+def writePDF(filename, page_or_file, output="output"):
     writer = PdfWriter()
-    writer.add_page(page)
+    writer.add_page(page_or_file)
+
+    if ".pdf" in filename:
+        filename = filename[:-4]
 
     try:
-        writer.write(f"./{OUTPUT_DIR}/{certificate_code[0]}.pdf")
+        writer.write(f"./{output}/{filename}.pdf")
     except:
-        os.mkdir(OUTPUT_DIR)
-        writer.write(f"./{OUTPUT_DIR}/{certificate_code[0]}.pdf")
+        os.mkdir(output)
+        writer.write(f"./{output}/{filename}.pdf")
 
 
-wb.save("final.xlsx")
+
+def main():
+    reader = PdfReader("Egypt foods - TQM Certificates.pdf")
+
+    wb = Workbook()
+    ws = wb.active
+
+    issue_date = "9/7/2023"
+    company_name = "Egypt Foods Group"
+    certificate_field = "Total Quality Management Training (TQM)"
+
+    titles = [
+        "certificate_holder",
+        "certificate_code",
+        "certificate_field",
+        "issue_date",
+        "company_name",
+    ]
+
+    ws.append(titles)
+
+    for page in reader.pages:
+        text = page.extract_text()
+
+        # Cert Holder
+        exp = re.compile(
+            r"It is hereby certified that\s*(.*?)\s*Attended the training on"
+        )
+        certificate_holder = exp.findall(text)
+        print(certificate_holder)
+
+        # Cert Code
+        exp = re.compile(r"\b[A-Z]{2}-[A-Z]{3}-\d{3}-\d{4}-\d{2}\b")
+        certificate_code = exp.findall(text)
+
+        certificate_dir = writePDF(certificate_code[0], page)
+        ws.append(
+            [
+                certificate_holder[0],
+                certificate_code[0],
+                certificate_field,
+                issue_date,
+                company_name,
+        )
+
+    wb.save("final.xlsx")
+
+
+if __name__ == "__main__":
+    main()
