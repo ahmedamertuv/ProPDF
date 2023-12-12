80271f7e85d9e34111adfeac4c65aca0c3dff211
JJAHMEDAMER <jjahmedamer@gmail.com>
2023-12-12 15:02:31 +0200

    add data to xlsx file

--- main.py
+++ main.py
@@ -1,23 +1,44 @@
-from PyPDF2 import PdfReader
+from PyPDF2 import PdfReader, PdfWriter
+from openpyxl import Workbook
 import re
 
 
 reader = PdfReader("Egypt foods - TQM Certificates.pdf")
-number_of_pages = len(reader.pages)
+wb = Workbook()
+ws = wb.active
 
-issue_date = '9/7/2023'
+issue_date = "9/7/2023"
 company_name = "Egypt Foods Group"
 certificate_field = "Total Quality Management Training (TQM)"
+
+titles = [
+    "certificate_holder",
+    "certificate_code",
+    "certificate_field",
+    "issue_date",
+    "company_name",
+]
+
+ws.append(titles)
 
 for page in reader.pages:
     text = page.extract_text()
 
     # Cert Holder
-    exp = re.compile(r'It is hereby certified that\s*(.*?)\s*Attended the training on')
+    exp = re.compile(r"It is hereby certified that\s*(.*?)\s*Attended the training on")
     certificate_holder = exp.findall(text)
     print(certificate_holder)
 
     # Cert Code
-    exp = re.compile(r'\b[A-Z]{2}-[A-Z]{3}-\d{3}-\d{4}-\d{2}\b')
-    certifictae_code = exp.findall(text)
-    print(certifictae_code)
+    exp = re.compile(r"\b[A-Z]{2}-[A-Z]{3}-\d{3}-\d{4}-\d{2}\b")
+    certificate_code = exp.findall(text)
+    ws.append(
+        [
+            certificate_holder[0],
+            certificate_code[0],
+            certificate_field,
+            issue_date,
+            company_name,
+        ]
+    )
+wb.save("final.xlsx")
